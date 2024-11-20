7d1322f08c949cbf63a6125c83da61f3d44fe639
romario <kulikr.js@mail.ru>
2024-11-21 00:05:15 +0300

    added getCaloriesFromInputs function

--- index.js
+++ index.js
@@ -1,487 +1,3 @@
-// step 69
-
-function padRow(rowNumber, rowCount) {
-    return " ".repeat(rowCount - rowNumber ) + character.repeat(rowNumber) + " ".repeat(rowCount - rowNumber);
-}
-
-// step 70
-
-function padRow(rowNumber, rowCount) {
-    return " ".repeat(rowCount - rowNumber) + character.repeat(2*rowNumber - 1) + " ".repeat(rowCount - rowNumber);
-}
-
-// step 71
-
-for (let i = 0; i < count; i += 1) {}
-
-// step 72
-
-for (let i = 0; i < count; i++) {}
-
-// step 73
-
-for (let i = 1; i < count; i++) {
-    rows.push(padRow(i + 1, count));
-}
-
-// step 74
-
-for (let i = 1; i < count; i++) {
-    rows.push(padRow(i, count));
-}
-
-// step 75
-
-for (let i = 1; i <= count; i++) {
-    rows.push(padRow(i, count));
-}
-
-// step 82
-
-if ("") {
-} else if(5 < 10) {
-    console.log("5 is less than 10");
-}
-
-// step 83
-
-if ("") {
-    console.log("Condition is true");
-} else if (5 > 10) {
-    console.log("5 is less than 10");
-} else {
-    console.log("This is the else block");
-}
-
-// step 88
-
-while (continueLoop) {
-    done++;
-    if(done == count){
-
-    }
-}
-
-// step 102
-
-for (let i = count; i > 0; i = i - 1) {
-    rows.push(padRow(i, count));
-}
-
-// step 103
-
-for (let i = count; i > 0; i -= 1) {
-    rows.push(padRow(i, count));
-}
-
-// step 104
-
-for (let i = count; i > 0; i--) {
-    rows.push(padRow(i, count));
-}
-
-// step 108
-
-const numbers = [1, 2, 3];
-const shifted = numbers.shift();
-console.log(shifted);
-
-const unshifted = numbers.unshift(5);
-console.log(unshifted);
-console.log(numbers);
-
-// step 113
-
-for (let i = 1; i <= count; i++) {
-    if(inverted) {
-        rows.unshift(padRow(i, count));
-    }
-}
-
-// step 116
-
-for (let i = 1; i <= count; i++) {
-    if (inverted) {
-        rows.unshift(padRow(i, count));
-    } else {
-        rows.push(padRow(i, count));
-    }
-}
-
-// Review JavaScript Fundamentals by Building a Gradebook App
-// step 1
-
-function getAverage(scores) {
-    const allScore = scores.reduce((acc, item) => {
-        return acc + item
-    }, 0)
-
-    return allScore / (scores.length);
-}
-
-console.log(getAverage([92, 88, 12, 77, 57, 100, 67, 38, 97, 89]));
-console.log(getAverage([45, 87, 98, 100, 86, 94, 67, 88, 94, 95]));
-
-// step 2
-
-function getGrade(score) {
-    if(score === 100) {
-        return "A++";
-    } else if( score >= 90 & score <= 99) {
-        return "A";
-    } else if( score >= 80 & score <= 89) {
-        return "B";
-    } else if( score >= 70 & score <= 79) {
-        return "C";
-    } else if( score >= 60 & score <= 69) {
-        return "D";
-    } else if( score >= 0 & score <= 59) {
-        return "F";
-    }
-}
-
-console.log(getGrade(96));
-console.log(getGrade(82));
-console.log(getGrade(56));
-
-// step 3
-
-function hasPassingGrade(score) {
-    if(score <= 56) {
-        return false;
-    } else {
-        return true;
-    }
-}
-
-
-console.log(hasPassingGrade(100));
-console.log(hasPassingGrade(53));
-console.log(hasPassingGrade(87));
-
-// step 4
-
-function studentMsg(totalScores, studentScore) {
-    let msg = '';
-
-    if(hasPassingGrade(studentScore)) {
-        msg = `Class average: ${getAverage(totalScores)}. Your grade: ${getGrade(studentScore)}. You passed the course.`
-    } else {
-        msg = `Class average: ${getAverage(totalScores)}. Your grade: ${getGrade(studentScore)}. You failed the course.`
-    }
-
-    return msg;
-}
-console.log(studentMsg([92, 88, 12, 77, 57, 100, 67, 38, 97, 89], 37));
-
-// Learn Basic JavaScript by Building a Role Playing Game
-
-function buyHealth() {
-
-    if(gold >= 10) {
-
-        gold -= 10;
-        health += 10;
-        goldText.innerText = gold;
-        healthText.innerText = health;
-
-    }
-}
-
-// game
-
-let xp = 0;
-let health = 100;
-let gold = 50;
-let currentWeaponIndex = 0;
-let fighting;
-let monsterHealth;
-let inventory = ["stick"];
-
-const button1 = document.querySelector('#button1');
-const button2 = document.querySelector("#button2");
-const button3 = document.querySelector("#button3");
-const text = document.querySelector("#text");
-const xpText = document.querySelector("#xpText");
-const healthText = document.querySelector("#healthText");
-const goldText = document.querySelector("#goldText");
-const monsterStats = document.querySelector("#monsterStats");
-const monsterName = document.querySelector("#monsterName");
-const monsterHealthText = document.querySelector("#monsterHealth");
-const weapons = [
-    { name: 'stick', power: 5 },
-    { name: 'dagger', power: 30 },
-    { name: 'claw hammer', power: 50 },
-    { name: 'sword', power: 100 }
-];
-const monsters = [
-    {
-        name: "slime",
-        level: 2,
-        health: 15
-    },
-    {
-        name: "fanged beast",
-        level: 8,
-        health: 60
-    },
-    {
-        name: "dragon",
-        level: 20,
-        health: 300
-    }
-]
-const locations = [
-    {
-        name: "town square",
-        "button text": ["Go to store", "Go to cave", "Fight dragon"],
-        "button functions": [goStore, goCave, fightDragon],
-        text: "You are in the town square. You see a sign that says \"Store\"."
-    },
-    {
-        name: "store",
-        "button text": ["Buy 10 health (10 gold)", "Buy weapon (30 gold)", "Go to town square"],
-        "button functions": [buyHealth, buyWeapon, goTown],
-        text: "You enter the store."
-    },
-    {
-        name: "cave",
-        "button text": ["Fight slime", "Fight fanged beast", "Go to town square"],
-        "button functions": [fightSlime, fightBeast, goTown],
-        text: "You enter the cave. You see some monsters."
-    },
-    {
-        name: "fight",
-        "button text": ["Attack", "Dodge", "Run"],
-        "button functions": [attack, dodge, goTown],
-        text: "You are fighting a monster."
-    },
-    {
-        name: "kill monster",
-        "button text": ["Go to town square", "Go to town square", "Go to town square"],
-        "button functions": [goTown, goTown, easterEgg],
-        text: 'The monster screams "Arg!" as it dies. You gain experience points and find gold.'
-    },
-    {
-        name: "lose",
-        "button text": ["REPLAY?", "REPLAY?", "REPLAY?"],
-        "button functions": [restart, restart, restart],
-        text: "You die. &#x2620;"
-    },
-    {
-        name: "win",
-        "button text": ["REPLAY?", "REPLAY?", "REPLAY?"],
-        "button functions": [restart, restart, restart],
-        text: "You defeat the dragon! YOU WIN THE GAME! &#x1F389;"
-    },
-    {
-        name: "easter egg",
-        "button text": ["2", "8", "Go to town square?"],
-        "button functions": [pickTwo, pickEight, goTown],
-        text: "You find a secret game. Pick a number above. Ten numbers will be randomly chosen between 0 and 10. If the number you choose matches one of the random numbers, you win!"
-    }
-];
-
-// initialize buttons
-button1.onclick = goStore;
-button2.onclick = goCave;
-button3.onclick = fightDragon;
-
-function update(location) {
-    button1.innerText = location["button text"][0];
-    button2.innerText = location["button text"][1];
-    button3.innerText = location["button text"][2];
-    button1.onclick = location["button functions"][0];
-    button2.onclick = location["button functions"][1];
-    button3.onclick = location["button functions"][2];
-    text.innerText = location.text;
-}
-
-function goTown() {
-    update(locations[0]);
-}
-
-function goStore() {
-    update(locations[1]);
-}
-
-function goCave() {
-    update(locations[2]);
-}
-
-function buyHealth() {
-    if (gold >= 10) {
-        gold -= 10;
-        health += 10;
-        goldText.innerText = gold;
-        healthText.innerText = health;
-    } else {
-        text.innerText = "You do not have enough gold to buy health.";
-    }
-}
-
-function buyWeapon() {
-    if (currentWeaponIndex < weapons.length - 1) {
-        if (gold >= 30) {
-            gold -= 30;
-            currentWeaponIndex++;
-            goldText.innerText = gold;
-            let newWeapon = weapons[currentWeaponIndex].name;
-            text.innerText = "You now have a " + newWeapon + ".";
-            inventory.push(newWeapon);
-            text.innerText += " In your inventory you have: " + inventory;
-        } else {
-            text.innerText = "You do not have enough gold to buy a weapon.";
-        }
-    } else {
-        text.innerText = "You already have the most powerful weapon!";
-        button2.innerText = "Sell weapon for 15 gold";
-        button2.onclick = sellWeapon;
-    }
-}
-
-function sellWeapon() {
-    if (inventory.length > 1) {
-        gold += 15;
-        goldText.innerText = gold;
-        let currentWeapon = inventory.shift();
-        text.innerText = "You sold a " + currentWeapon + ".";
-        text.innerText += " In your inventory you have: " + inventory;
-    } else {
-        text.innerText = "Don't sell your only weapon!";
-    }
-}
-
-function fightSlime() {
-    fighting = 0;
-    goFight();
-}
-
-function fightBeast() {
-    fighting = 1;
-    goFight();
-}
-
-function fightDragon() {
-    fighting = 2;
-    goFight();
-}
-
-function goFight() {
-    update(locations[3]);
-    monsterHealth = monsters[fighting].health;
-    monsterStats.style.display = "block";
-    monsterName.innerText = monsters[fighting].name;
-    monsterHealthText.innerText = monsterHealth;
-}
-
-function attack() {
-    text.innerText = "The " + monsters[fighting].name + " attacks.";
-    text.innerText += " You attack it with your " + weapons[currentWeaponIndex].name + ".";
-    health -= monsters[fighting].level;
-    monsterHealth -= weapons[currentWeaponIndex].power + Math.floor(Math.random() * xp) + 1;
-
-}
-
-function dodge() {
-    text.innerText = "You dodge the attack from the " + monsters[fighting].name;
-}
-
-function defeatMonster() {
-    gold += Math.floor(monsters[fighting].level * 6.7);
-    xp += monsters[fighting].level;
-    goldText.innerText = gold;
-    xpText.innerText = xp;
-    update(locations[4]);
-}
-
-function lose() {
-    update(locations[5]);
-}
-
-function restart() {
-    xp = 0;
-    health = 100;
-    gold = 50;
-    currentWeaponIndex = 0;
-    inventory = ["stick"];
-    goldText.innerText = gold;
-    healthText.innerText = health;
-    xpText.innerText = xp;
-    goTown();
-}
-
-function easterEgg() {
-    update(locations[7]);
-}
-
-function pickTwo() {
-    pick(2);
-}
-
-function pickEight() {
-    pick(8);
-}
-
-function pick(guess) {
-    const numbers = [];
-    while (numbers.length < 10) {
-        numbers.push(Math.floor(Math.random() * 11));
-    }
-    text.innerText = "You picked " + guess + ". Here are the random numbers:\n";
-    for (let i = 0; i < 10; i++) {
-        text.innerText += numbers[i] + "\n";
-    }
-    if (numbers.includes(guess)) {
-        text.innerText += "Right! You win 20 gold!";
-        gold += 20;
-        goldText.innerText = gold;
-    } else {
-        text.innerText += "Wrong! You lose 10 health!";
-        health -= 10;
-        healthText.innerText = health;
-        if (health <= 0) {
-            lose();
-        }
-    }
-}
-
-// next project
-
-const darkColorsArr = [
-    "#2C3E50",
-    "#34495E",
-    "#2C2C2C",
-    "#616A6B",
-    "#4A235A",
-    "#2F4F4F",
-    "#0E4B5A",
-    "#36454F",
-    "#2C3E50",
-    "#800020",
-];
-
-function getRandomIndex() {
-    const randomIndex = Math.floor(darkColorsArr.length * Math.random());
-    return randomIndex;
-}
-
-const body = document.querySelector("body");
-const bgHexCodeSpanElement = document.querySelector("#bg-hex-code");
-
-function changeBackgroundColor() {
-    const color = darkColorsArr[getRandomIndex()];
-
-    bgHexCodeSpanElement.innerText = color;
-    body.style.backgroundColor = color;
-}
-const btn = document.querySelector("#btn");
-
-btn.onclick = changeBackgroundColor();
-
-
-// Calorie counter
-
 const calorieCounter = document.getElementById('calorie-counter');
 const budgetNumberInput = document.getElementById('budget');
 const entryDropdown = document.getElementById('entry-dropdown');
@@ -497,5 +13,53 @@ function cleanInputString(str) {
 
 function isInvalidInput(str) {
     const regex = /\d+e\d+/i;
+    return str.match(regex);
+}
+
+function addEntry() {
+    const targetInputContainer = document.querySelector(`#${entryDropdown.value} .input-container`);
+    const entryNumber = targetInputContainer.querySelectorAll('input[type="text"]').length + 1;
+    const HTMLString = `
+  <label for="${entryDropdown.value}-${entryNumber}-name">Entry ${entryNumber} Name</label>
+  <input type="text" id="${entryDropdown.value}-${entryNumber}-name" placeholder="Name" />
+  <label for="${entryDropdown.value}-${entryNumber}-calories">Entry ${entryNumber} Calories</label>
+  <input
+    type="number"
+    min="0"
+    id="${entryDropdown.value}-${entryNumber}-calories"
+    placeholder="Calories"
+  />`;
+    targetInputContainer.insertAdjacentHTML('beforeend', HTMLString);
+}
+
+function calculateCalories(e) {
+    e.preventDefault();
+    isError = false;
+
+    const breakfastNumberInputs = document.querySelectorAll("#breakfast input[type='number']");
+    const lunchNumberInputs = document.querySelectorAll("#lunch input[type='number']");
+    const dinnerNumberInputs = document.querySelectorAll("#dinner input[type='number']");
+    const snacksNumberInputs = document.querySelectorAll("#snacks input[type='number']");
+    const exerciseNumberInputs = document.querySelectorAll("#exercise input[type='number']");
+
 
 }
+
+function getCaloriesFromInputs(list) {
+    let calories = 0;
+
+    for (const item of list) {
+        const currVal = cleanInputString(item.value);
+        const invalidInputMatch = isInvalidInput(currVal);
+
+        if (invalidInputMatch) {
+            alert(`Invalid Input: ${invalidInputMatch[0]}`);
+            isError = true;
+            return null;
+        }
+        calories += Number(currVal);
+    }
+    return calories;
+}
+
+addEntryButton.addEventListener("click", addEntry);
